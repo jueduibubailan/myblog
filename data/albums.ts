f839225df0a503ca85a982b4d754b90fd91b6ce9
jueduibubailan <3106674136@qq.com>
2026-08-21 02:51:55 +0800

    Auto sync source code for Vercel 🚀

--- data/albums.ts
+++ data/albums.ts
@@ -4,6 +4,12 @@ export interface Album { id: string; title: string; description: string; cover: 
 
 export const albums: Album[] = [
   {
+    "cover": "https://upload-bbs.miyoushe.com/upload/2025/08/22/429668945/4f722c349ced9ceb0e0081f75be9c9c5_8234239911221306624.png?x-oss-process=image//resize,s_600/quality,q_80/auto-orient,0/interlace,1/format,png",
+    "id": "album_1787250263038",
+    "photos": [],
+    "date": "2026-08-20"
+  },
+  {
     "cover": "https://upload-bbs.miyoushe.com/upload/2025/07/04/291110358/fc5bd69d0186169a7f8a576557e52981_1867527063979254496.png?x-oss-process=image//resize,s_600/quality,q_80/auto-orient,0/interlace,1/format,png",
     "id": "album_1787237630214",
     "photos": [],
--- siteConfig.ts
+++ siteConfig.ts
@@ -31,7 +31,7 @@ export const siteConfig = {
   photoWallImage: "https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg",
   cloudMusicIds: ["2155423467"],
   social: {
-    github: "https://github.com/ jueduibubailan",
+    github: "https://github.com/jueduibubailan",
     gitee: "",
     google: "",
     email: "wjz_Wh1te_Rice@163.com",
@@ -48,10 +48,10 @@ export const siteConfig = {
   // 👇 【新增】：全局背景弹幕配置
   danmakuList: ["在干嘛呢？", "有笨蛋嘛？", "前方高能反应！", "GROMACS 跑起来了吗？", "MD 模拟什么时候才能出图啊", "Graph Neural Networks 炼丹中...", "BUG 修复进度 99%", "今天背单词了吗？", "Tailwind CSS 拯救前端", "写算法中", "睡大觉中", "到底在干嘛？"],
   gitalkConfig: {
-    clientID: "",
-    clientSecret: "",
-    repo: "",
-    owner: "",
+    clientID: "Ov23liLSwK27k7baM1rq",
+    clientSecret: "c8a2e0672c21b5b1224bf3e9b53657d80471ab64",
+    repo: "1",
+    owner: "jueduibubailan",
     admin: [""],
   },
   buildDate: "2026-03-23T00:00:00", // 建站日期
